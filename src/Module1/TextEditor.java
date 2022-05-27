d2bb3fd7ea233b6d4165b74e11b8ef86d2aeb5b5
League Student <league@iMac-24.attlocal.net>
2022-05-26 18:29:17 -0700

    Module 1 checkpoint (ip)
    Seems that this didn't save the first time.

--- src/Module1/TextEditor.java
+++ src/Module1/TextEditor.java
@@ -1,18 +1,23 @@
 package Module1;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
-public class TextEditor {
+public class TextEditor implements ActionListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JButton save = new JButton();
 	JButton load = new JButton();
 	JTextArea textArea = new JTextArea(50, 50);
+	FileWriter fw;
 	
 	TextEditor(){
 		frame.add(panel);
@@ -25,7 +30,28 @@ public class TextEditor {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		save.setText("Save");
 		load.setText("Load");
+		save.addActionListener(this);
+		load.addActionListener(this);
 		
 		frame.pack();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource()==save) {
+			try {
+				fw = new FileWriter("src/Module1/editableText.txt");
+				fw.write(textArea.getText());
+				fw.close();
+			}
+			catch(IOException e1) {
+				e1.printStackTrace();
+			}
+			
+		}
+		else {
+			
+		}
 	}
 }
